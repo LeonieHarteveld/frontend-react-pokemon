06fce0215fbaaadfcfaea73363710f7bce769da1
leharteveld <l.e.harteveld@gmail.com>
2026-06-08 22:33:58 +0200

    loading state en AbortController toegevoegd.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import PokemonDetails from "./components/PokemonDetails/PokemonDetails.jsx";
 import {useEffect, useState} from "react";
 import axios from "axios";
+import logo from "../src/assets/logo.jpg"
 
 
 function App() {
@@ -10,47 +11,64 @@ function App() {
     const [error, toggleError] = useState(false)
     const [offset, setOffset] = useState(0);
     const [nextPage, setNextPage] = useState(null);
+    const [loading, setLoading] = useState(true);
     const url = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`;
 
+    useEffect(() => {
+        const controller = new AbortController();
 
     async function getPokemon() {
         toggleError(false)
+        setLoading(true);
         try {
-            const result = await axios.get(url);
+            const result = await axios.get(url, {
+                signal: controller.signal,
+            });
+
             setPokemon(result.data.results);
             setNextPage(result.data.next);
         } catch (e) {
             console.error(e)
             toggleError(true)
+        } finally {
+            setLoading(false);
         }
     }
-
-    useEffect(() => {
         getPokemon();
+
+        return () => {
+            controller.abort();
+        };
     }, [offset]);
 
+
+
     return (
-        <>
+        <div className="outer-container">
+
+            <div><img src={logo} alt="pokemon logo"/></div>
 
             <h1>Gotta catch em all!</h1>
+
+            {loading && <p>Pokémon worden geladen...</p>}
             {error && <p>Er ging iets mis. Probeer het opnieuw.</p>}
 
-            {offset >= 20 &&
-                <button
-                    onClick={() => setOffset(offset - 20)}>
-                    Vorige 20 Pokémon
-                </button>}
+            <div className="button-wrapper">
+                {offset >= 20 &&
+                    <button
+                        onClick={() => setOffset(offset - 20)}>
+                        Vorige 20 Pokémon
+                    </button>}
 
-            {nextPage && (
-                <button
-                    onClick={() => setOffset(offset + 20)}>
-                    Volgende 20 Pokémon
-                </button>)
-            }
+                {nextPage && (
+                    <button
+                        onClick={() => setOffset(offset + 20)}>
+                        Volgende 20 Pokémon
+                    </button>)
+                }
+            </div>
 
-
-
-
+            {!loading && (
             <div className="pokemon-cards">
                 {pokemons.map(pokemon => (
                     <PokemonDetails
@@ -60,7 +78,8 @@ function App() {
                     />
                 ))}
             </div>
-        </>
+            )}
+        </div>
 
     )
 }
